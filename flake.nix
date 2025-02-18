63b743d7198cfb1a1a815610cf4ea3f34a4e9b77
Odd E. Ebbesen <oddebb@gmail.com>
2025-02-18 23:47:03 +0100

    Good enough for a start

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Not in use yet, might need to be under home-manager config instead?
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,25 +19,25 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nixvim }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      # Think I want to just have the bare minimum shell tools here, and solve 
-      # the rest with home-manager.
-      environment.systemPackages = with pkgs; [ 
-          fzf
-          fzf-git-sh
-          fzf-zsh
+    let
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        # Think I want to just have the bare minimum shell tools here, and solve 
+        # the rest with home-manager.
+        environment.systemPackages = with pkgs; [
           htop
-          neovim
-          tmux
-          tmuxPlugins.tmux-fzf
-          zsh
-          zsh-forgit
-          zsh-fzf-history-search
-          zsh-fzf-tab
+          vim
           git
+          zsh
+          #tmux
+          #tmuxPlugins.tmux-fzf
+          #zsh-forgit
+          #zsh-fzf-history-search
+          #zsh-fzf-tab
+          #fzf
+          #fzf-git-sh
+          #fzf-zsh
           #brave
           #cloudflare-warp
           #cloudflared
@@ -48,77 +49,92 @@
           #resilio-sync
           #signal-desktop
           #keepassxc
-      ];
-
-      # This would overwrite /etc/shells, so leaving this commented for now
-      # environment.shells = [ pkgs.zsh ];
-
-      # I'd like to not use homebrew, but nice to have a fallback if there are packages 
-      # not available in nix
-      homebrew = {
-        enable = true;
-        casks = [
-          #"wezterm"
         ];
-      };
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # This would overwrite /etc/shells, so leaving this commented for now
+        # environment.shells = [ pkgs.zsh ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh = {
-        enable = true;  
-      };
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Touch-id for sudo
-      security.pam.enableSudoTouchIdAuth = true;
-
-      system = {
-        keyboard.enableKeyMapping = true;
-        keyboard.remapCapsLockToEscape = true;
-
-        defaults = {
-          finder = {
-            AppleShowAllExtensions = true;
-            ShowPathbar = true;
-            FXEnableExtensionChangeWarning = false;
-            _FXShowPosixPathInTitle = true;
-          };
+        # I'd like to not use homebrew, but nice to have a fallback if there are packages 
+        # not available in nix
+        homebrew = {
+          enable = false;
+          casks = [
+            #"wezterm"
+          ];
         };
-        # Tab between form controls and F-row that behaves as F1-F12
-        # Note: gives me error about NSGlobalDomain not existing
-        # NSGlobalDomain = {
-        #   AppleKeyboardUIMode = 3;
-        #   "com.apple.keyboard.fnState" = true;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh = {
+          enable = true;
+        };
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # Touch-id for sudo
+        security.pam.enableSudoTouchIdAuth = true;
+
+        system = {
+          keyboard.enableKeyMapping = true;
+          keyboard.remapCapsLockToEscape = true;
+
+          defaults = {
+            finder = {
+              AppleShowAllExtensions = true;
+              ShowPathbar = true;
+              FXEnableExtensionChangeWarning = false;
+              _FXShowPosixPathInTitle = true;
+            };
+          };
+          # Tab between form controls and F-row that behaves as F1-F12
+          # Note: gives me error about NSGlobalDomain not existing
+          # NSGlobalDomain = {
+          #   AppleKeyboardUIMode = 3;
+          #   "com.apple.keyboard.fnState" = true;
+          # };
+        };
+
+        # users.users.oddee = {
+        #     name = "oddee";
+        #     home = "/Users/oddee";
         # };
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#$hostname
+      darwinConfigurations = {
+        "odd-mbp-m1" = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            configuration
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = false;
+                useUserPackages = true;
+                users.oddee = import ./modules/home-manager/home.nix;
+              };
+              users.users.oddee.home = "/Users/oddee";
+            }
+          ];
+          specialArgs = { inherit inputs; };
+        };
       };
 
-      users.users.oddee = {
-          name = "oddee";
-          home = "/Users/oddee";
-      };
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."odd-mbp-m1".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."odd-mbp-m1" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."odd-mbp-m1".pkgs;
-  };
 }
---
+++ modules/home-manager/home.nix
@@ -0,0 +1,340 @@
+{ config, pkgs, lib, ... }:
+{
+  # For all possible options, see: https://nix-community.github.io/home-manager/options.xhtml
+
+  home = {
+    stateVersion = "25.05";
+
+    language = {
+      address = "sv_SE.UTF-8";
+      base = "sv_SE.UTF-8";
+      collate = "sv_SE.UTF-8";
+      ctype = "sv_SE.UTF-8";
+      measurement = "sv_SE.UTF-8";
+      messages = "sv_SE.UTF-8";
+      monetary = "sv_SE.UTF-8";
+      name = "sv_SE.UTF-8";
+      numeric = "sv_SE.UTF-8";
+      paper = "sv_SE.UTF-8";
+      telephone = "sv_SE.UTF-8";
+      time = "sv_SE.UTF-8";
+    };
+
+    # Extra entries to add to PATH
+    sessionPath = [ ];
+
+    sessionVariables = {
+      GOPATH = "$HOME/gopath";
+      GOWS = "$HOME/ResilioSync/code/go/github.com/oddlid";
+      RUSTWS = "$HOME/ResilioSync/code/rust";
+      # EDITOR = "nvim"; # will be set by the neovim config below
+      VISUAL = "$EDITOR";
+      LESS = "FRi"; # --quit-if-one-screen --RAW-CONTROL-CHARS --ignore-case
+      PAGER = "less";
+      MANPAGER = "nvim +Man!";
+      REPORTTIME = "5";
+      TIMEFMT = "%U user, %S system, %P cpu, %*Es total";
+    };
+
+    # TODO: see if I need to specify these, as it seems those enabled via programs.* don't need an entry here
+    packages = with pkgs; [
+      bat
+      cargo
+      fzf
+      fzf-git-sh
+      fzf-zsh
+      ripgrep
+      tmuxPlugins.tmux-fzf
+      zsh-forgit
+      zsh-fzf-history-search
+      zsh-fzf-tab
+    ];
+
+    shell = {
+      enableBashIntegration = true;
+      enableShellIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    shellAliases = {
+      _tm = "tmux -u2 attach-session || tmux -u2";
+    };
+
+    username = "oddee";
+  };
+
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableVteIntegration = true;
+      autosuggestion = {
+        enable = true;
+        strategy = [
+          "history"
+        ];
+      };
+      history = {
+        append = false;
+        expireDuplicatesFirst = true;
+        extended = true;
+        save = 100000;
+        saveNoDups = true;
+        share = true;
+      };
+      historySubstringSearch = {
+        enable = true;
+      };
+      localVariables = { };
+      oh-my-zsh = {
+        enable = true;
+        extraConfig = ''
+          zstyle :omz:plugins:ssh-agent identities id_rsa id_ed25519 github
+        '';
+        plugins = [
+          "alias-finder"
+          "aliases"
+          "dircycle"
+          "docker"
+          "encode64"
+          "extract"
+          "fzf"
+          "git"
+          "golang"
+          "helm"
+          "isodate"
+          "macos"
+          "nmap"
+          "rsync"
+          "safe-paste"
+          "systemadmin"
+          "tmux"
+          "transfer"
+          "universalarchive"
+          "urltools"
+        ];
+        theme = "crcandy";
+      };
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [
+          "brackets"
+        ];
+        patterns = { };
+        styles = { };
+      };
+    };
+
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      enableVteIntegration = true;
+    };
+
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        batgrep
+        batwatch
+      ];
+    };
+
+    dircolors = {
+      enable = true;
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    fd = {
+      enable = true;
+      hidden = true;
+      ignores = [
+        ".git/"
+        "*.bak"
+      ];
+    };
+
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      changeDirWidgetCommand = "fd --type d";
+      changeDirWidgetOptions = [
+        "--preview 'tree -C {} | head -200'"
+      ];
+      # Taken from the Solarized example here: https://github.com/junegunn/fzf/wiki/Color-schemes
+      # In tmux, terminal must be tmux-256color, and the term overrides must also be set as below for it to work properly
+      colors = {
+        "bg+" = "#073642";
+        bg = "#002b36";
+        spinner = "#719e07";
+        hl = "#586e75";
+        fg = "#839496";
+        header = "#586e75";
+        info = "#cb4b16";
+        pointer = "#719e07";
+        marker = "#719e07";
+        "fg+" = "#839496";
+        prompt = "#719e07";
+        "hl+" = "#719e07";
+      };
+      defaultCommand = "fd --type f";
+      fileWidgetCommand = "fd --type f";
+      tmux = {
+        enableShellIntegration = true;
+      };
+    };
+
+    git = {
+      enable = true;
+      aliases = {
+        st = "status";
+        ls-untracked = "!git ls-files --others --exclude-standard";
+      };
+      delta = {
+        enable = true;
+      };
+      # Cannot be enabled at the same time as delta
+      # diff-highlight = {
+      #   enable = true;
+      # };
+      # diff-so-fancy = {
+      #   enable = true;
+      # };
+      extraConfig = { };
+      lfs.enable = true;
+      userEmail = "oddebb@gail.com";
+      userName = "Odd E. Ebbesen";
+    };
+
+    gpg = {
+      enable = true;
+    };
+
+    htop = {
+      enable = true;
+      settings = { };
+    };
+
+    jq = {
+      enable = true;
+    };
+
+    # Just keeping it for reference
+    k9s = {
+      enable = false;
+    };
+
+    lazygit = {
+      enable = true;
+      settings = { };
+    };
+
+    less = {
+      enable = true;
+    };
+
+    lesspipe = {
+      enable = true;
+    };
+
+    lf = {
+      enable = true;
+      cmdKeybindings = { };
+      commands = { };
+      keybindings = { };
+      settings = { };
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    nix-your-shell = {
+      enable = true;
+    };
+
+    ripgrep = {
+      enable = true;
+    };
+
+    # For reference, to checkout later
+    sftpman = {
+      enable = false;
+    };
+
+    tmux = {
+      enable = true;
+      clock24 = true;
+      escapeTime = 40;
+      focusEvents = true;
+      historyLimit = 5000;
+      mouse = true;
+      newSession = true;
+      prefix = "C-x";
+      sensibleOnTop = false; # see: https://github.com/nix-community/home-manager/issues/5952
+      # shell = "${pkgs.zsh}/bin/zsh"; # the shell option in extraConfig seems to work better
+      terminal = "tmux-256color"; # This makes the fzf colorscheme work properly
+
+      plugins = with pkgs.tmuxPlugins; [
+        pain-control
+        logging
+        resurrect
+        yank
+      ];
+
+      extraConfig = ''
+        bind-key x send-prefix
+        bind-key C-x last-window
+        set -g default-command "$SHELL"
+        set -g default-shell "$SHELL"
+        set -g terminal-overrides ',xterm-256color:Tc'
+        set -as terminal-overrides ',xterm*:sitm=\E[3m'
+        # allow for navigating between words with option
+        set-window-option -g xterm-keys on
+        # Allow the arrow key to be used immediately after changing windows
+        set -g repeat-time 0
+        # Set window notifications
+        set -g monitor-activity on
+        set -g visual-activity on
+        # Status update interval
+        set -g status-interval 1
+        set -g renumber-windows on    # renumber windows when a window is closed
+        # recommendations for vim-tpipeline
+        set -g status-style bg=default
+        set -g status-left-length 90
+        set -g status-right-length 90
+        set -g status-justify centre
+        set -g status-right '#{prefix_highlight}#(hostname) | %Y-%m-%d %H:%M'
+        set-window-option -g window-status-separator " "
+        set-window-option -g window-status-current-format "#[fg=colour66]#W"
+        set-window-option -g window-status-format "#W"
+      '';
+    };
+
+    # TODO: check if it's better to enable it here, or via global packages, and how to set up nix-mac-util, or whatsitsname...
+    wezterm = {
+      enable = false;
+    };
+  };
+
+  services = {
+    gpg-agent = {
+      enable = false;
+    };
+
+    # TODO: checkout - some gmail sync service
+    lieer = {
+      enable = false;
+    };
+  };
+
+  # TODO: have a look at the options in target.darwin, as they contain a lot of settings for macOS
+
+}
