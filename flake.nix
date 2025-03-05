0f446a18360af1fc356f90850eb1d112c5c70920
Odd E. Ebbesen <oddebb@gmail.com>
2025-03-05 23:36:46 +0100

    Changed name for sudo touchID, as suggested by nix

--- flake.nix
+++ flake.nix
@@ -191,7 +191,8 @@
         nixpkgs.hostPlatform = "aarch64-darwin";
 
         # Touch-id for sudo
-        security.pam.enableSudoTouchIdAuth = true;
+        #security.pam.enableSudoTouchIdAuth = true;
+        security.pam.services.sudo_local.touchIdAuth = true;
 
         system = {
           keyboard.enableKeyMapping = true;
