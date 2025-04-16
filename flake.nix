7afcdc67aac0606239018a96fb186da7af4d8260
Odd E. Ebbesen <git@oddware.net>
2025-04-16 13:03:22 +0200

    Disabled linux-builder

--- flake.nix
+++ flake.nix
@@ -189,7 +189,7 @@
         nix = {
           #optimize.automatic = true; # suggested, but doesn't exist...
           linux-builder = {
-            enable = true;
+            enable = false;
             ephemeral = true;
             # Not quite sure how this option works in this context, but when trying to build conduwuit for aarch64-linux in the builder,
             # I found out, after hours of googling, that I need to pass "--max-jobs 0" to nix build, to be sure _only_ the VM does the building.
