f0ed26b8f8d2498f43280290abd7406f0e2aa79f
Odd E. Ebbesen <git@oddware.net>
2025-10-03 15:15:22 +0200

    Set up substituters and trusted-public-keys

--- flake.nix
+++ flake.nix
@@ -293,6 +293,12 @@
                 "@admin"
                 "oddee"
               ];
+              substituters = [
+                "https://cache.lix.systems"
+              ];
+              trusted-public-keys = [
+                "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+              ];
             };
           };
 
